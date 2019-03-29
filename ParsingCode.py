a824b331256550a76b6da22b7310f9fa148c1fad
Joseph Lupton <43359031+jlupdawg@users.noreply.github.com>
2019-03-29 17:15:00 -0500

    Cleaned up comments
    Cleaned up comments. Finished Code.

--- ParsingCode.py
+++ ParsingCode.py
@@ -1,33 +1,36 @@
 #This code takes user input and splits it into a list of strings and a list of integers.
 #Use a comma (',') as the delimiter
 
-stringInput = input()
+stringInput = input()                           #input a string of seperated by a delimeter like "Hello,793.5, World,78"
 print("You input: " + stringInput)
-stringArray = stringInput.split(',')
-numberArray = [] 
+stringArray = stringInput.split(',')            #change the delimeter here if you need to
+numberArray = []                                #iniitalize the number array
 
-def is_float(s):
+def is_float(s):                                #function for determining if the input is a float
     try:
         float(s)
         return True
     except ValueError:
         return False
-def is_int(s):
+def is_int(s):                                  #function for determining if the input is an int
     try:
         int(s)
         return True
     except ValueError:
         return False
 
-for i in stringArray:
+for i in stringArray:                           #for every element in the stringArray
     print(i)
-    if is_float(i) & is_int(i):
-        numberArray.append(int(i))
-    elif is_float(i):
-        numberArray.append(float(i))
+    if  is_int(i):                              #because ints are considered floats check ints first (if you check floats first 
+                                                #it will double count. You must use the exact type to properly remove the ints from the
+                                                #string list
+        numberArray.append(int(i))              #if it passes both then make it an int
+        
+    elif is_float(i):                           #else if it is a float 
+        numberArray.append(float(i))            #append it to the array as a float
         
 for i in numberArray:
-    stringArray.remove(str(i))
+    stringArray.remove(str(i))                  #remove all of the numbers from the strings list
     
 print(stringArray)
 print(numberArray)
--- ParsingFunction.py
+++ ParsingFunction.py
@@ -1,29 +1,29 @@
-def parseStrings(stringInput, delimiter): #inputs are the string and the delimeter
-    stringArray = stringInput.split(delimiter) #split the string at every delimter and make it into a list
-    numberArray = [] #initialize an empty number array
+def parseStrings(stringInput, delimiter):               #inputs are the string and the delimeter
+    stringArray = stringInput.split(delimiter)          #split the string at every delimter and make it into a list
+    numberArray = []                                    #initialize an empty number array
 
-    def is_float(s): #function for determining if the input is a float
+    def is_float(s):                                    #function for determining if the input is a float
         try:
             float(s)
             return True
         except ValueError:
             return False
-    def is_int(s): #function for determining if the input is an int
+    def is_int(s):                                      #function for determining if the input is an int
         try:
             int(s)
             return True
         except ValueError:
             return False
 
-    for i in stringArray: #for every element in the stringArray
-        if is_int(i): #because ints are considered floats check ints first (if you check floats first 
-                      #it will double count. You must use the exact type to properly remove the ints from the
-                      #string list
-            numberArray.append(int(i)) #if it passes both then make it an int
-        elif is_float(i):  #else if it is a float 
-            numberArray.append(float(i)) #append it to the array as a float
+    for i in stringArray:                               #for every element in the stringArray
+        if is_int(i):                                   #because ints are considered floats check ints first (if you check floats first 
+                                                        #it will double count. You must use the exact type to properly remove the ints from the
+                                                        #string list
+            numberArray.append(int(i))                  #if it passes both then make it an int
+        elif is_float(i):                               #else if it is a float 
+            numberArray.append(float(i))                #append it to the array as a float
             
     for i in numberArray: 
-        stringArray.remove(str(i)) #remove all of the numbers from the strings list
+        stringArray.remove(str(i))                      #remove all of the numbers from the strings list
         
-    return stringArray, numberArray #return both lists
+    return stringArray, numberArray                     #return both lists
