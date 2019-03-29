986578d1f578f83f690b61fe98e1e6a2f2aadbca
Joseph Lupton <43359031+jlupdawg@users.noreply.github.com>
2019-03-29 17:09:30 -0500

    Created an external function

---
+++ exampleOfFunction.py
@@ -0,0 +1,9 @@
+from ParsingFunction import parseStrings #Import the parsing function file
+
+delimeter = ',' #define the delimeter you use in your string
+myString = "Hello,798.5,World,15" #insert any string seperated by a delimeter
+
+string,number = parseStrings(myString,delimeter) #returns the string list and then the int/float list
+
+print(string)
+print(number)
